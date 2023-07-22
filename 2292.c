be5640393027d03afc259b88b50c194be698dc2c
kxunn <139880640+kxunn@users.noreply.github.com>
2023-07-22 22:11:50 +0900

    Add files via upload

---
+++ 2292.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	int a = 1;
+	int max = 1;
+	int add = 6;
+
+	while (1) {
+		if (n <= max) break;
+
+		max += add;
+		add += 6;
+		++a;
+	}
+
+	printf("%d\n", a);
+	return 0;
+}
